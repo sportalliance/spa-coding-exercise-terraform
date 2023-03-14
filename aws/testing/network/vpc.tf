2031bd707291aadbfc11b9b8f83d8afdfd941ecd
Eike Lurz <eike.lurz@sportalliance.com>
2023-03-14 08:34:16 +0100

    🔧 Add VPC assumptions

---
+++ aws/testing/network/vpc.tf
@@ -0,0 +1,4 @@
+# Assume a VPC exists with a public subnet and 3 private subnets.
+# Each subnet has a route table.
+# The public subnet as an internet gateway and the private subnets have a NAT each.
+# Each NAT has an elatstic IP.
